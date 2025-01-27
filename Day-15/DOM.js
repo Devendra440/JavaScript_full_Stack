d1aba762893756e4166a1fb15883f51cb1aaa87c
Devendra440 <pinnamdevendragupta@gmail.com>
2025-01-27 21:53:57 +0530

    first commit

---
+++ Day-15/DOM.js
@@ -0,0 +1,29 @@
+// let mainEle = document.createElement("div");
+// mainEle.setAttribute("id" , "mainBlock");
+// console.log(mainEle);
+
+// let topEle = document.createElement("div");
+// topEle.setAttribute("class" , "topBlock");
+
+// let image = document.createElement("img");
+// image.src="https://cdn.pixabay.com/photo/2024/11/11/17/39/dog-9190504_640.jpg";
+// image.width="100";
+// image.height="100";
+// image.style.objectFit="cover"
+
+// let bottomEle = document.createElement("div");
+// bottomEle.setAttribute("class" , "bottomBlock");
+
+// let h1 = document.createElement("hi");
+// h1.innerText="CAT";
+
+// let btn = document.createElement("button");
+// btn.innerText="view More";
+
+// bottomEle.appendChild(h1);
+// bottomEle.appendChild(btn);
+// topEle.appendChild(image);
+// mainEle.appendChild(topEle);
+// mainEle.appendChild(bottomEle);
+
+// document.body.appendChild(mainEle)
